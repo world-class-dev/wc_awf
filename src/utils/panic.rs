a094bff6ae3007dc4f324e5c95aff81707e38ad3
wc-ai <wc-ai@users.noreply.github.com>
2026-08-30 13:50:58 +0300

    clean: remove cfg feature check for panic hook

--- src/utils/panic.rs
+++ src/utils/panic.rs
@@ -1,8 +1,6 @@
 pub fn set_panic_hook() {
-    #[cfg(target_arch = "wasm32")]
-    {
-        std::panic::set_hook(Box::new(|info| {
-            web_sys::console::error_1(&info.to_string().into());
-        }));
-    }
+    // Standard std panic hook for WASM / native builds
+    std::panic::set_hook(Box::new(|info| {
+        println!("Panic occurred: {:?}", info);
+    }));
 }
